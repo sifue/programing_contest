5b0a5d2f5874ec6dd127a4699d82caba085c7c26
Soichiro Yoshimura <yoshimura@soichiro.org>
2013-03-20 02:31:16 +0900

    コメントを追加

--- src/DepthFirstSearch.scala
+++ src/DepthFirstSearch.scala
@@ -4,7 +4,7 @@ object DepthFirstSearch {
     val a = List(1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 ,8, 13)
     val k = 444
 
-    // リストの要素を最初から足すか足さないかという2択で考え
+    // リストの要素を最初から足すか足さないかという2択で考え、４つの場合なら
     // 足さない, 足さない, 足さない, 足さない
     // 足さない, 足さない, 足さない, 足す
     // 足さない, 足さない, 足す, 足さない
@@ -21,21 +21,25 @@ object DepthFirstSearch {
       // インデックスが最後のnに達していたら判定する
       // addedList.foldLeft(0)(_+_) はリストの中身を全部足して集約するという意味
       if (index == n) if (addedList.foldLeft(0)(_+_) == k) {
-        println("YES (%s)".format(addedList))
+        println("YES (%s)".format(addedList)) // 見つかったら内訳を出力して終了
         return true
       } else {
         return false
       }
 
       // インデックスが最後のnに達していない場合
-      // 今回を足さない場合と判定して残りを再帰的に判定する
+      // 今回を足さない場合と判定して残りを再帰的に判定し、trueの時は終了
       if (depthFirstSeach(index + 1, addedList)) return true
-      // 今回を足す場合と判定して残りを再帰的に判定する
+      // 今回を足す場合と判定して残りを再帰的に判定し、trueの時は終了
       // addedList :+ a(index)はリストの末尾に要素を一つ足すという意味
       if (depthFirstSeach(index + 1, addedList :+ a(index))) return true
-      // 全てのパターンを探しても見つからなかった時はfalse
+      // 全てのパターンを探しても見つからなかった時はfalse　
       return false
     }
-    depthFirstSeach(0, List())
+
+    // 実行
+    if (!depthFirstSeach(0, List())){
+      println("NO") // 最終的に見つからなかった場合はNOを出力
+    }
   }
 }
