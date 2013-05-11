b7ff7ae83bbebe20b858d434ef809cd3c9c41a14
Soichiro Yoshimura <yoshimura@soichiro.org>
2013-05-11 20:41:24 +0900

    コメントを追加

--- src/Expedition.scala
+++ src/Expedition.scala
@@ -2,6 +2,7 @@ import scala.collection.mutable
 
 object Expedition {
 
+  // ガソリンスタンドとゴールを同一視する
   trait Spot {def position():Int; def gas:Int}
   case class GasStand(position:Int, gas:Int) extends Spot
   case class Goal(position:Int, gas:Int) extends Spot
@@ -20,15 +21,20 @@ object Expedition {
 
     // 燃料が多い順に並ぶプライオリティーキューを用意
     val priorityQueue = new mutable.PriorityQueue[Spot]()(GasOrder)
-
     val answers = new mutable.MutableList[Spot]
     var tank = 10
     var currentPosition = 0
-    var isReachable = true
+    var isReachableFinally = true
+
+    // 一つ一つのガソリンスタンドを進め最後のSpot(Goalまでたどり着けるかを判定)
+    // 次のスポットの距離まで辿りつけない場合は、今辿りつけたガソリンスタンドで
+    // 最も補給できたところから補給する。それでたどり着けるようになればゴール
+    // 可能な補給を全て行ったとしても辿り着けなかった場合は最終的に辿りつけなかったとする
     spotList.foreach(s => {
-        if(!isReachable) return
+        if(!isReachableFinally) return
         val nextDistance = s.position - currentPosition
 
+        // 次に辿り着けない場合は、燃料が多い順に補給
         def isReachableNow = {tank - nextDistance >= 0}
         def isRefillableNow = {!priorityQueue.isEmpty}
         while(!isReachableNow && isRefillableNow){
@@ -43,7 +49,7 @@ object Expedition {
           priorityQueue.enqueue(s)
         } else {
           // 可能な限り補給してもダメな場合は辿りつけなかったとして終了
-          isReachable = false
+          isReachableFinally = false
         }
       }
     )
